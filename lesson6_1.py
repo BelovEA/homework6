1a1b728b56b1872a733529ce150ae55c8a153be3
Professional <sssemkiqqq@mail.ru>
2022-08-21 18:52:47 +0300

    task 1 is ready!

---
+++ lesson6_1.py
@@ -0,0 +1,14 @@
+import time
+class TrafficLight:
+    _color = "Red, Green, Yellow"
+    def running(self):
+        while True:
+            print("Red")
+            time.sleep(7)
+            print("Green")
+            time.sleep(2)
+            print("Yellow")
+            time.sleep(5)
+a = TrafficLight()
+print(a.running())
+
