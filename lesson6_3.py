bf9c48da7dc9c61e174bc5045fced8ffa98a81ac
Professional <sssemkiqqq@mail.ru>
2022-08-22 22:44:48 +0300

    task 3 is ready!

--- lesson6_3.py
+++ lesson6_3.py
@@ -12,4 +12,7 @@ class Position(Worker):
 
     def get_total_income(self):
         return sum(self._income.values())
+guy = Position("Nice", "Guy", "Nuwbie", 40000, 20000)
+print(f"вот этот парень - {guy.get_full_name()}")
+print(f"а вот его зарплата - {guy.get_total_income()}")
 
