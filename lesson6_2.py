c66d99e77d2d9e9bc7182c13d8f3defc61d45af9
Professional <sssemkiqqq@mail.ru>
2022-08-22 07:24:59 +0300

    task 2 is ready

--- lesson6_2.py
+++ lesson6_2.py
@@ -2,26 +2,8 @@ class Road:
     def __init__(self, length, width):
         self._length = length
         self._width = width
-class TypeA(Road):
-    def __init__(self, length, width):
-        self._length = length
-        self._width = width
-        mass = 25,7
-        calc = length * width * mass * 5
-        return calc
-class TypeB(Road):
-    def __init__(self, length, width):
-        self._length = length
-        self._width = width
-        mass = 24,6
-        calc = length * width * mass * 5
-        return calc
-class TypeC(Road):
-    def __init__(self, length, width):
-        self._length = length
-        self._width = width
-        mass = 24,9
-        calc = length * width * mass * 5
-        return calc
-a1 = TypeA(int(input()), int(input()))
-print(TypeA)
+    def calc(self, weight =26, thickness = 5):
+        return f"{self._length * self._width * weight * thickness / 1000}"
+
+road = Road(2000, 14)
+print(road.calc())
